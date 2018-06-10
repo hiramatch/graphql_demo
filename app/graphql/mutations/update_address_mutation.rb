bfbb8186d057b6d4bf600a2e5a0e85da99e23615
hiramatch <hiramatch@gmail.com>
2018-06-10 12:33:46 +0900

    add AddressInput

--- app/graphql/mutations/update_address_mutation.rb
+++ app/graphql/mutations/update_address_mutation.rb
@@ -1,17 +1,19 @@
 Mutations::UpdateAddressMutation = GraphQL::Relay::Mutation.define do
   name 'UpdateAddress'
 
-  input_field :postal_code, !types.Int
-  input_field :address, !types.String
+  # input_field :postal_code, !types.Int
+  # input_field :address, !types.String
+  input_field :address_input, !Types::AddressInputType
 
   return_field :address, !Types::AddressType
   return_field :user, Types::UserType
 
   resolve ->(_obj, inputs, ctx) {
     begin
+      address_input = inputs.address_input
       address = ctx[:current_user].address
-      address.postal_code = inputs.postal_code
-      address.address = inputs.address
+      address.postal_code = address_input.postal_code
+      address.address = address_input.address
       address.save
     rescue => e
       return GraphQL::ExecutionError.new(e.message)
